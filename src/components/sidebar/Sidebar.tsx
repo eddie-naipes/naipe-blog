2d67b898268b7bca673671149a326cd57d3c4de9
edmilson.dias@mozaiko.io <eddiasdev@gmail.com>
2025-04-14 18:22:17 -0300

    add avatar image and profile edit link to sidebar component

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
 import styles from "./Sidebar.module.css"
 import bgwhale from "../../assets/animeWhale.png"
+import {PencilLine} from "@phosphor-icons/react";
 
 export const Sidebar = () => {
+
+    const linkPictureProfile = "http://github.com/eddie-naipes.png"
+
     return (
         <aside className={styles.sidebar}>
             <img
@@ -10,12 +14,18 @@ export const Sidebar = () => {
                 className={styles.cover}
             />
             <div className={styles.profile}>
+                <img
+                    src={linkPictureProfile}
+                    alt="Picture Profile"
+                    className={styles.avatar}
+                />
                 <strong>Eddie Dias</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
